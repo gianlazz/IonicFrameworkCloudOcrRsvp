e6185643286bcac7cd885e1470739bfdf9e5c05f
Gian Lazzarini <gianlazzarini@gmail.com>
2017-07-27 11:00:14 -0700

    Changed text on walkthrough slides to placeholder subjects

--- src/pages/walkthrough/walkthrough.ts
+++ src/pages/walkthrough/walkthrough.ts
@@ -16,18 +16,18 @@ export class WalkthroughPage {
 
   slides = [
     {
-      title: "Welcome to the Docs!",
-      description: "The <b>Ionic Component Documentation</b> showcases a number of useful components that are included out of the box with Ionic.",
+      title: "Premis Of The App!",
+      description: "The <b>details</b>.",
       image: "assets/img/ica-slidebox-img-1.png",
     },
     {
-      title: "What is Ionic?",
-      description: "<b>Ionic Framework</b> is an open source SDK that enables developers to build high quality mobile apps with web technologies like HTML, CSS, and JavaScript.",
+      title: "AR Cloud OCR",
+      description: "<b>Details</b> .",
       image: "assets/img/ica-slidebox-img-2.png",
     },
     {
-      title: "What is Ionic Cloud?",
-      description: "The <b>Ionic Cloud</b> is a cloud platform for managing and scaling Ionic apps with integrated services like push notifications, native builds, user auth, and live updating.",
+      title: "Rapid Serial Visual Presentation",
+      description: "The <b>description</b>.",
       image: "assets/img/ica-slidebox-img-3.png",
     }
   ];
