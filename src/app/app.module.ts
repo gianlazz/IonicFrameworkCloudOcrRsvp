f430bfd48cdc9e33534b7f0a1f6ef06ec4a0a345
Gian Lazzarini <gianlazzarini@gmail.com>
2017-07-27 10:08:06 -0700

    Declaired walkthrough in app.module.ts and linked it to home page
    http://blog.ionic.io/10-minutes-with-ionic-2-adding-pages-and-navigation/

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,11 +6,13 @@ import { StatusBar } from '@ionic-native/status-bar';
 
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
+import { WalkthroughPage } from '../pages/walkthrough/walkthrough';
 
 @NgModule({
   declarations: [
     MyApp,
-    HomePage
+    HomePage,
+    WalkthroughPage
   ],
   imports: [
     BrowserModule,
@@ -19,7 +21,8 @@ import { HomePage } from '../pages/home/home';
   bootstrap: [IonicApp],
   entryComponents: [
     MyApp,
-    HomePage
+    HomePage,
+    WalkthroughPage
   ],
   providers: [
     StatusBar,
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import {WalkthroughPage} from '../walkthrough/walkthrough';
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
-
+  walkthroughPage = WalkthroughPage;
   constructor(public navCtrl: NavController) {
 
   }
