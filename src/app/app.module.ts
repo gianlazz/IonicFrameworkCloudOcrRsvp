d022c350b6cc668b1718eb4a5ffc4503c5c2a2bf
Gian Lazzarini <gianlazzarini@gmail.com>
2017-07-29 15:14:28 -0700

    Declaired rsvp page in app.module.ts and linked to home page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,12 +7,14 @@ import { StatusBar } from '@ionic-native/status-bar';
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
 import { WalkthroughPage } from '../pages/walkthrough/walkthrough';
+import { RsvpPage } from '../pages/rsvp/rsvp';
 
 @NgModule({
   declarations: [
     MyApp,
     HomePage,
-    WalkthroughPage
+    WalkthroughPage,
+    RsvpPage
   ],
   imports: [
     BrowserModule,
@@ -22,7 +24,8 @@ import { WalkthroughPage } from '../pages/walkthrough/walkthrough';
   entryComponents: [
     MyApp,
     HomePage,
-    WalkthroughPage
+    WalkthroughPage,
+    RsvpPage
   ],
   providers: [
     StatusBar,
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
-import {WalkthroughPage} from '../walkthrough/walkthrough';
+import { WalkthroughPage } from '../walkthrough/walkthrough';
+import { RsvpPage } from '../rsvp/rsvp';
 
 @Component({
   selector: 'page-home',
@@ -8,6 +9,7 @@ import {WalkthroughPage} from '../walkthrough/walkthrough';
 })
 export class HomePage {
   walkthroughPage = WalkthroughPage;
+  rsvpPage = RsvpPage;
   constructor(public navCtrl: NavController) {
 
   }
