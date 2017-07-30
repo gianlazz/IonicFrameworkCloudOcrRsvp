d4080eaf7f18b65fc5ad9f15b86f456b50b1a6e6
Gian Lazzarini <gianlazzarini@gmail.com>
2017-07-30 16:39:39 -0700

    demoString

--- src/pages/rsvp/rsvp.ts
+++ src/pages/rsvp/rsvp.ts
@@ -15,6 +15,7 @@ import { NavController, NavParams } from 'ionic-angular';
 export class RsvpPage {
 
   rsvpString: string;
+  demoString: string = "Angular is a TypeScript-based open-source front-end web application platform led by the Angular Team at Google and by a community of individuals and corporations to address all of the parts of the developer's workflow while building complex web applications. Angular is a complete rewrite from the same team that built AngularJS.";
   constructor(public navCtrl: NavController, public navParams: NavParams) {
   this.rsvpString = "Hello World";
   }
