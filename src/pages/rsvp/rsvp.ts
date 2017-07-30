ea410cd185b1c9bf02c99f01851ccb58e1a5abea
Gian Lazzarini <gianlazzarini@gmail.com>
2017-07-30 16:43:28 -0700

    Built on example code at SeattleJS Hackers meetup
    https://github.com/lordvon/SpeedyReader

--- src/pages/rsvp/rsvp.ts
+++ src/pages/rsvp/rsvp.ts
@@ -12,17 +12,59 @@ import { NavController, NavParams } from 'ionic-angular';
   selector: 'page-rsvp',
   templateUrl: 'rsvp.html',
 })
-export class RsvpPage {
 
+export class RsvpPage {
   rsvpString: string;
   demoString: string = "Angular is a TypeScript-based open-source front-end web application platform led by the Angular Team at Google and by a community of individuals and corporations to address all of the parts of the developer's workflow while building complex web applications. Angular is a complete rewrite from the same team that built AngularJS.";
   constructor(public navCtrl: NavController, public navParams: NavParams) {
-  this.rsvpString = "Hello World";
   this.rsvpString = this.demoString;
+
   }
 
-  ionViewDidLoad() {
+  private ionViewDidLoad() {
     console.log('ionViewDidLoad RsvpPage');
   }
+
+  /*
+  private getWords(text)
+  {
+  	//return text.split(/\b\s+/)
+  	//return text.split(" ")
+  	return text.split(/[\s]+/)
+  }
+  private changeWord(wordstring)
+  {
+  	$("#word").html(wordstring)
+  }
+
+  //READING = 0
+  displayNextWord(words,index,interval) {
+  	var $container = $("#word");
+  	(function step() {
+  		//if(READING == 0){return;}
+  		$container.html(words[index]);
+  		if (index++ < words.length) {
+  			setTimeout(step, interval);
+  		}
+  	})();
+  }
+
+    private runRSVP(){
+      let displayNextWord = this.displayNextWord;
+    $(document).ready(function(){
+    	$("#wordDisplay").click(function(){
+        console.log("#wordDisplay");
+    		//if(READING == 1){ READING = 0;return; }
+    		let text = $("#inputText").val()
+    		let words = getWords(text)
+    		let speed = $('#wpm').val()
+    		let interval = 1.0/(speed/60.0)*1000.0
+    		let READING = 1
+    		displayNextWord(words, 0, interval);
+    		READING = 0
+    	})
+    })
+  }
+  */
 
 }
