09c8f4fd4181b532f425f14408b801a23eaa4434
Gian Lazzarini <gianlazzarini@gmail.com>
2017-07-30 14:03:40 -0700

    Initialized rsvpString

--- src/pages/rsvp/rsvp.ts
+++ src/pages/rsvp/rsvp.ts
@@ -14,6 +14,7 @@ import { NavController, NavParams } from 'ionic-angular';
 })
 export class RsvpPage {
 
+  rsvpString: string;
   constructor(public navCtrl: NavController, public navParams: NavParams) {
   }
 
